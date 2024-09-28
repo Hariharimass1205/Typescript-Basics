d10a896ce0c5870549986b53690d52b164e8ba43
Hariharan <hariharimass1205@gmail.com>
2024-09-28 15:47:31 +0530

    fn3

--- function/example2.ts
+++ function/example2.ts
@@ -10,3 +10,21 @@ function adds2(a:number,b:number): number {
    return a+b+432
 }
 console.log(adds(1,2))
+
+
+
+function checks(num:number):boolean | string{
+    if(num<5){
+        return true
+    }
+    return "200 ok"
+}
+console.log(checks(53))
+// "|" is used as and in return part
+
+
+let arr:number[]=[1,2,3,4,5]
+arr.map((a:number):number=>{
+    return a
+})
+//syntex for arr fn
